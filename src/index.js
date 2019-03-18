2080a6f0f9136a79f32fa83f5b8934083315c001
ctellezesp <a01274912@itesm.mx>
2019-03-17 19:08:02 -0600

    Example of Fetch

--- src/index.js
+++ src/index.js
@@ -10,3 +10,33 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+class Fetch extends React.Component{
+	constructor(props){
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick(){
+		fetch('https://yvone.github.io/DAW/some-numbers.json').then((response)=>{
+			return response.json()
+		}).then((data)=>{
+			console.log(data);
+			let pares = data.filter(function(elem){
+				return (elem%2)===0;
+			});
+			console.log(pares);
+		});
+	}
+
+	render(){
+		return(
+			<button onClick={this.handleClick}>Data</button>
+		);
+	}
+}
+
+ReactDOM.render(
+	<Fetch />,
+	document.getElementById('root')
+);
